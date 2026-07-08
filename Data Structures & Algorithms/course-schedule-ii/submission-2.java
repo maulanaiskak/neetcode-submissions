04533bf09926f2daea42bcbfaf4d30c9798b1d11
Maulana Iskak <75124297+maulanaiskak@users.noreply.github.com>
2026-07-08 21:30:00 +0700

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] findOrder(final int numCourses, final int[][] prerequisites) {
+        final var graph = new ArrayList<List<Integer>>();
+        final var indegree = new int[numCourses];
+
+        for (var course = 0; course < numCourses; course++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (final var prerequisite : prerequisites) {
+            final var course = prerequisite[0];
+            final var prerequisiteCourse = prerequisite[1];
+
+            graph.get(prerequisiteCourse).add(course);
+            indegree[course]++;
+        }
+
+        final Queue<Integer> queue = new ArrayDeque<>();
+
+        for (var course = 0; course < numCourses; course++) {
+            if (indegree[course] == 0) {
+                queue.offer(course);
+            }
+        }
+
+        final var order = new int[numCourses];
+        var index = 0;
+
+        while (!queue.isEmpty()) {
+            final var course = queue.poll();
+            order[index++] = course;
+
+            for (final var nextCourse : graph.get(course)) {
+                indegree[nextCourse]--;
+
+                if (indegree[nextCourse] == 0) {
+                    queue.offer(nextCourse);
+                }
+            }
+        }
+
+        return index == numCourses ? order : new int[0];
+    }
+}
