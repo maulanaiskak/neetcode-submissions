1efee369a53094c45f9e855b9e6b923a3c224549
Maulana Iskak <75124297+maulanaiskak@users.noreply.github.com>
2026-09-05 19:40:16 +0700

    Add: duplicate-integer - submission-13

---
+++ Data Structures & Algorithms/duplicate-integer/submission-13.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        var set = new HashSet<Integer>();
+
+        for (var num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
